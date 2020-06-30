3818a3c77bc8e6c94b4eefa73a7050c4f3105058
Segun <shegunolanitori@gmail.com>
2020-06-30 12:30:04 +0100

    cors url

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({
     extended: false
 }));
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://employeez.vercel.app',
     credentials: true
 }))
 // 'http://localhost:3000'
